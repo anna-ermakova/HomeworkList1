5708bf36521d5b7465ee0d4c9009fa35baac814a
anna-ermakova <ibkxbirf79@>
2022-12-19 22:00:20 +0300

    ДЗ 3. Задание 3 паспорт

--- src/Multiplication/Main.java
+++ src/Multiplication/Main.java
@@ -19,7 +19,7 @@ public class Main {
         Set<Multiplication> multiplications = new HashSet<>();
         Random random = new Random();
         while (multiplications.size() < 15) {
-            //Multiplication multiplication = new Multiplication(random.nextInt(10), random.nextInt(10));
+            //Multiplication multiplication = new Multiplication(random.nextInt(10), random.nextInt(10));//нет возможности указать начальное значение диапазона-2
             Multiplication multiplication = new Multiplication(getRandomNumberUsingInts(2, 10), getRandomNumberUsingInts(2, 10));
             multiplications.add(multiplication);
             System.out.println(multiplications);
